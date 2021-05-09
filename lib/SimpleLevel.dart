ed2c6628b72246920ed7b9e029b64dcf71cee5fb
saaqii <nomidharaja73237@gmail.com>
2021-05-09 12:54:56 +0500

    simple level

---
+++ lib/SimpleLevel.dart
@@ -0,0 +1,210 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'menu.dart';
+import 'utils.dart';
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+
+}
+
+class Simplelevel extends StatelessWidget {
+  static final String title = 'Simple Level Game';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: title,
+    theme: ThemeData(
+      primaryColor: Colors.blue,
+    ),
+    home: MainPage(title: title
+
+    ),
+  );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class Player {
+  static const none = '';
+  static const X = 'X';
+  static const O = 'O';
+}
+
+
+class _MainPageState extends State<MainPage> {
+  static final countMatrix = 3;
+  static final double size = 92;
+
+  String lastMove = Player.none;
+  List<List<String>> matrix;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setEmptyFields();
+  }
+
+  void setEmptyFields() => setState(() => matrix = List.generate(
+    countMatrix,
+        (_) => List.generate(countMatrix, (_) => Player.none),
+  ));
+
+  Color getBackgroundColor() {
+    final thisMove = lastMove == Player.X ? Player.O : Player.X;
+
+    return getFieldColor(thisMove).withAlpha(150);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: getBackgroundColor(),
+    appBar: AppBar(
+      title: Text(widget.title),
+      leading: new IconButton(
+        icon: new Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Menu()),
+          );
+        },
+      ),
+
+    ),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: Utils.modelBuilder(matrix, (x, value) => buildRow(x)),
+    ),
+  );
+
+  Widget buildRow(int x) {
+    final values = matrix[x];
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: Utils.modelBuilder(
+        values,
+            (y, value) => buildField(x, y),
+      ),
+    );
+  }
+
+  Color getFieldColor(String value) {
+    switch (value) {
+      case Player.O:
+        return Colors.blue;
+      case Player.X:
+        return Colors.red;
+      default:
+        return Colors.white;
+    }
+  }
+
+  Widget buildField(int x, int y) {
+    final value = matrix[x][y];
+    final color = getFieldColor(value);
+
+    return Container(
+      margin: EdgeInsets.all(4),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(size, size),
+          primary: color,
+        ),
+        child: Text(value, style: TextStyle(fontSize: 32)),
+        onPressed: () => selectField(value, x, y),
+      ),
+    );
+
+  }
+
+  void selectField(String value, int x, int y) {
+    if (value == Player.none) {
+      final newValue = lastMove == Player.X ? Player.O : Player.X;
+
+      setState(() {
+        lastMove = newValue;
+        matrix[x][y] = newValue;
+      });
+
+      if (isWinner(x, y)) {
+        showEndDialog('Player $newValue Won');
+      } else if (isEnd()) {
+        showEndDialog('Undecided Game');
+      }
+    }
+  }
+
+  bool isEnd() =>
+      matrix.every((values) => values.every((value) => value != Player.none));
+
+
+  bool isWinner(int x, int y) {
+    var col = 0, row = 0, diag = 0, rdiag = 0;
+    final player = matrix[x][y];
+    final n = countMatrix;
+
+    for (int i = 0; i < n; i++) {
+      if (matrix[x][i] == player) col++;
+      if (matrix[i][y] == player) row++;
+      if (matrix[i][i] == player) diag++;
+      if (matrix[i][n - i - 1] == player) rdiag++;
+    }
+
+    return row == n || col == n || diag == n || rdiag == n;
+
+  }
+
+  int _n = 1;
+  void add() {
+    setState(() {
+      _n++;
+    });
+  }
+  void crash(int _n) {
+    if (_n == 5) {
+      exit(0);
+    }
+  }
+  Future showEndDialog(String title) => showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text('Press to Restart the Game'),
+      actions: [
+        ElevatedButton(
+          onPressed:  () {
+            add();
+            setEmptyFields();
+            Navigator.of(context).pop();
+            crash(_n);
+          },
+
+          child: new Text('You have Played ${_n} Times, Press to Restart',
+              style: new TextStyle(fontSize: 20.0)),
+
+        ),
+
+      ],
+    ),
+  );
+
+}
