e463095313cfd771804a3cdf0ca0299de2c7332c
saaqii <nomidharaja73237@gmail.com>
2021-05-09 12:56:10 +0500

    drwaer

---
+++ lib/drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        child: Padding(
+          padding: EdgeInsets.only(top: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/profile.PNg'),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                "SAQIB JAVAID",
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                "FA17-BCS-096",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 20.0,
+      ),
+      //Now let's Add the button for the Menu
+      //and let's copy that and modify it
+
+
+    ]);
+  }
+}
